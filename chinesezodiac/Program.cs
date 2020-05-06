8c0085fb55408807b407031c93d6923be1ffcd91
Park Yejin <s2018s30@e-mirim.hs.kr>
2020-05-06 20:15:30 +0900

    초기 커밋.

---
+++ chinesezodiac/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chinesezodiac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+            switch(year%12)
+            {
+                case 0:
+                    Console.WriteLine("원숭이");
+                    break;
+                case 1:
+                    Console.WriteLine("닭");
+                    break;
+                case 2:
+                    Console.WriteLine("개");
+                    break;
+                case 3:
+                    Console.WriteLine("돼지");
+                    break;
+                case 4:
+                    Console.WriteLine("쥐");
+                    break;
+                case 5:
+                    Console.WriteLine("소");
+                    break;
+                case 6:
+                    Console.WriteLine("호랑이");
+                    break;
+                case 7:
+                    Console.WriteLine("토끼");
+                    break;
+                case 8:
+                    Console.WriteLine("용");
+                    break;
+                case 9:
+                    Console.WriteLine("뱀");
+                    break;
+                case 10:
+                    Console.WriteLine("말");
+                    break;
+                case 11:
+                    Console.WriteLine("양");
+                    break;
+            }
+        }
+    }
+}
